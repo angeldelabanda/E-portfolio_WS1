06571e287cfb1af2ba4a05fe949e83bb63c1d4ba
angeldelabanda <adelabanda2024@hccci.edu.ph>
2026-07-22 12:26:01 +0800

    Enhance portfolio script with new features
    Added a click event to change the welcome message and updated font style for contacts.

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function showWelcomeMessage() {
     window.alert('Welcome sa portfolio ni Angel!!!!');
 }
 
+//Click my name to see the message =>
+function clickHere() {
+  document.getElementById("welcome-heading").innerHTML = "Hi! Welcome to my portfolio! I'm Angel <3";
+}
+
+//Dark modeee
 function toggleTheme() {
     document.body.classList.toggle('dark');
     const button = document.getElementById('theme-toggle');
@@ -11,10 +17,7 @@ function toggleTheme() {
         button.textContent = document.body.classList.contains('dark') ? 'Light Mode ☀️' : 'Dark Mode 🌙';
     }
 }
-
-window.onload = function () {
-    showWelcomeMessage();
-    console.log('Page loaded successfully');
-};
+//change font styleeee sang contacts
+document.getElementById("contact").style.fontFamily = "monospace, monospace";
 
 document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
